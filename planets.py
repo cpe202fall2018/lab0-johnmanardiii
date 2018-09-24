ab67b3fc4ecba5eb57d7bda34013ce06f92a3afd
John Manard <jmanard@calpoly.edu>
2018-09-23 21:14:34 -0700

    Reduced casting down to one line

--- planets.py
+++ planets.py
@@ -1,7 +1,6 @@
 def weight_on_planets():
    # write your code here
-   weight = input('What do you weigh on earth? ')  # Asks the user for their weight.
-   weight = float(weight)
+   weight = float(input('What do you weigh on earth? '))  # Asks the user for their weight.
    mars = weight * .38  # Converts to weight on Mars.
    jupiter = weight * 2.34  # Converts to weight on Jupiter.
    print("\nOn Mars you would weigh " + str(mars) + " pounds.\n"\
