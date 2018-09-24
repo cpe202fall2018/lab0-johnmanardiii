6ea77b0efe1c5895a8d5c4327bd340e95b41f5ad
John Manard <jmanard@calpoly.edu>
2018-09-24 10:13:38 -0700

    Reduced casting to one

--- planets.py
+++ planets.py
@@ -3,8 +3,8 @@ def weight_on_planets():
    weight = float(input('What do you weigh on earth? '))  # Asks the user for their weight.
    mars = weight * .38  # Converts to weight on Mars.
    jupiter = weight * 2.34  # Converts to weight on Jupiter.
-   print("\nOn Mars you would weigh " + str(mars) + " pounds.\n"\
-         "On Jupiter you would weigh " + str(jupiter) + " pounds.", end='')
+   print("\nOn Mars you would weigh", mars, "pounds.\n"\
+         "On Jupiter you would weigh", jupiter, "pounds.", end='')
    
 if __name__ == '__main__':
    weight_on_planets()
